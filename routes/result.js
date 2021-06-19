a092514ab01032e5c0b79685ad8b2adce6c9727d
eddie <mreddiedu@gmail.com>
2021-06-18 19:01:39 -0700

    result function

--- routes/result.js
+++ routes/result.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.post('/', function(req, res, next) {
-  res.render('result', { title: 'resultPOST' });
+  res.render('result', { title: 'resultPOST', result: req });
 });
 
 router.get('/', function(req, res, next) {
