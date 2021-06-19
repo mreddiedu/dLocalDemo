02415e3e91d65f41a64ad793d87eed3c0c6753e7
eddie <mreddiedu@gmail.com>
2021-06-18 18:05:54 -0700

    adding result

--- routes/result.js
+++ routes/result.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'result' });
+  res.render('result', { title: 'result' });
 });
 
 module.exports = router;
